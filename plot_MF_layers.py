47de29c579e3a420c437efbaf95e3a9245ec76cd
Mike Fienen <mnfienen@usgs.gov>
2014-06-17 15:32:50 -0500

    Got rid of second subplot (now one per page). Allow specification of vertical exaggeration (VE). Use XML for input

--- plot_MF_layers.py
+++ plot_MF_layers.py
@@ -3,49 +3,49 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from matplotlib.backends.backend_pdf import PdfPages
-from collections import defaultdict
 import discomb_utilities
 import binaryfile_recarray as bf
-import pdb
+import sys
+import xml.etree.ElementTree as ET
 
-def set_rowscols(interval,nrows_cols,inputsname):
-    try:
-        start,end=map(int,inputs[inputsname].strip('[]').split(','))
-        if start>0:
-            start,end=start-1,end
-        else:
-            start,end=0,end
-    except:
-        start,end=0,nrows_cols-1
-    if interval>(end-start):
-        interval=end-start    
-    return start,end,interval
- 
+
+
+newparams = {'legend.fontsize':12, 'axes.labelsize':12, 'xtick.labelsize':12, 'ytick.labelsize':12,
+             'font.sans-serif':'Univers 57 Condensed',
+             'pdf.fonttype':42, 'pdf.compression':0}
+
+mpl.rcParams.update(newparams)
+
+
+# figure out vertical exaggeration
+def vert_exaggeration(subplot,Xunits):
+
+    defaultaspect=subplot.bbox.bounds[3]/subplot.bbox.bounds[2]
+    ylen=subplot.get_ylim()[1]-subplot.get_ylim()[0]
+    xlen=subplot.get_xlim()[1]-subplot.get_xlim()[0]
+
+    if Xunits=='miles':
+        VE=defaultaspect*(xlen*5280.0/ylen)
+    else:
+        VE=defaultaspect*(xlen/ylen)
+    return VE
+
+def tf2flag(intxt):
+    # converts text written in XML file to True or False flag when parsing XML input
+    if intxt.lower()=='true':
+        return True
+    else:
+        return False
+
 def make_imshow(layer,array,title):
     plt.figure()
     plt.imshow(array[layer,:,:])
     plt.colorbar()
     plt.title('%s %s' %(title,layer))
 
-
-def make_planimages(fnames,arrays,titles):
-    # figs: list of variable names, one for each multi-page PDF
-    # arrays: list of numpy ndarrays, one for each multi-page PDF
-    # titles: list of titles, one for each multi-page PDF
-    
-    nlay=len(arrays[0]) # number of model layers
-    
-    for c in range(len(fnames)):
-        print fnames[c]
-        fig=PdfPages(fnames[c])
-        for i in range(nlay):
-            make_imshow(i,arrays[c],titles[c])
-            fig.savefig()
-        fig.close()
-
-
-def make_xsectplot(nlayers,slice1,slice2,interval,slice1_ind,X,label,xlabel,colors,**kwargs):
+def make_xsectplot(nlayers,slice1,slice1_ind,X,label,xlabel,colors,VE,**kwargs):
     # colors: list of html colors, one for each layer going from top to bottom
     try:
         Xunits=kwargs["Xunits"]
@@ -73,9 +73,18 @@ def make_xsectplot(nlayers,slice1,slice2,interval,slice1_ind,X,label,xlabel,colo
         WTthick = 1.0
 
     fig=plt.figure()
-    ax1=plt.subplot(211)
-    ax2=plt.subplot(212) 
-    
+    ax = plt.subplot(111)
+    if VE < 0:
+        VE=vert_exaggeration(ax,Xunits)
+    else:
+        VE_x_conversion = 1.0
+        if Xunits.lower() == 'cells':
+            VE_x_conversion = 1/np.mean(DX)
+        elif Xunits.lower() == 'miles':
+            VE_x_conversion = 1/5280
+        #VE *= VE_x_conversion
+
+        
     for c in range(nlayers)[:-1]:
         
         # in case there aren't enough colors
@@ -85,63 +94,36 @@ def make_xsectplot(nlayers,slice1,slice2,interval,slice1_ind,X,label,xlabel,colo
         except IndexError:
             facecolor=colors[-1]
             
-        ax1.fill_between(X,slice1[:,c-1],slice1[:,c],facecolor=facecolor,linewidth=linewidth)
+        ax.fill_between(X,slice1[:,c-1],slice1[:,c],facecolor=facecolor,linewidth=linewidth)
 
         if c==0:
             # make room for label
-            ax1.set_ylim(ax1.get_ylim()[0],ax1.get_ylim()[1]*1.1)             
-            
-        # figure out vertical exaggeration
-        def vert_exaggeration(subplot,Xunits):
-            
-            defaultaspect=subplot.bbox.bounds[3]/subplot.bbox.bounds[2]
-            ylen=subplot.get_ylim()[1]-subplot.get_ylim()[0]
-            xlen=subplot.get_xlim()[1]-subplot.get_xlim()[0]
-            
-            if Xunits=='miles':
-                VE=defaultaspect*(xlen*5280.0/ylen)
-            else:
-                VE=defaultaspect*(xlen/ylen)
-            return VE
-        
-        VE1=vert_exaggeration(ax1,Xunits)
+            ax.set_ylim(ax.get_ylim()[0],ax.get_ylim()[1]*1.1)             
 
-        ax1.set_ylabel('Elevation (ft.)')
-        ax1.text(0.99,0.95,label %(slice1_ind+1,round(VE1,1)),verticalalignment='top',horizontalalignment='right',transform=ax1.transAxes)
+
+        plt.ylabel('Elevation (ft.)')
+        plt.xlabel(xlabel)
+        ax.text(0.99,0.95,label %(slice1_ind+1,VE),verticalalignment='top',
+                 horizontalalignment='right',transform=ax.transAxes)
         
         
-        if slice2<>None:
-            ax2.fill_between(X,slice2[:,c-1],slice2[:,c],facecolor=facecolor,linewidth=linewidth)
-            
-            if c==0:
-                # make room for label
-                ax2.set_ylim(ax2.get_ylim()[0],ax2.get_ylim()[1]*1.1)
-                
-            VE2=vert_exaggeration(ax2,Xunits)
-            ax2.set_ylabel('Elevation (ft.)')
-            ax2.text(0.99,0.95,label %(slice1_ind+1+interval,round(VE2,1)),verticalalignment='top',horizontalalignment='right',transform=ax2.transAxes)
-            #ax2.set_xlabel('Distance along section (miles)')
-                
-            ax2.set_xlabel('%s' %(xlabel))       
-        
+
+
         if Xunits=='cells':
-            tickz=ax1.get_xticks()
+            tickz=ax.get_xticks()
             # convert back to cells
             tickz_cells=[]
             for t in tickz:
                 idx = np.argmin(np.abs(X - t)) # find cell closest to the tick distance
                 tickz_cells.append(idx)
-            
-            ax1.set_xticklabels(tickz_cells)
-            ax2.set_xticklabels(tickz_cells)
 
-    if watertable == "On":
-        ax1.plot(X, WT1, linewidth=WTthick, color=WTcolor)
-        if slice2<>None:
-            ax2.plot(X, kwargs["WT2"], linewidth=WTthick, color=WTcolor)
+            ax.set_xticklabels(tickz_cells)
 
+        if watertable == "On":
+            ax.plot(X, WT1, linewidth=WTthick, color=WTcolor)
+        ax.set_aspect(VE, adjustable='box')
 
-def make_xsections(arrays,watertable,DX,DY,rc_range,row_interval,col_interval,fnames,colors,**kwargs):
+def make_xsections(arrays,watertable,DX,DY,rc_range,row_interval,col_interval,fnames,colors,VE,**kwargs):
     # arrays: list of numpy ndarrays to plot
     # DX,DY: column/row coordinates (in ft. or miles)
     # interval: index interval at which to make slices
@@ -175,15 +157,11 @@ def make_xsections(arrays,watertable,DX,DY,rc_range,row_interval,col_interval,fn
         
         print "generating cross section plots at:"
         print "Rows: ",
-        for c in range(nrows)[startrow:endrow:row_interval][::2]:
+        for c in range(startrow,endrow,row_interval):
             print "%s,%s" %(c+1,c+row_interval+1),
 
             cslice1=np.transpose(arrays[i][:,c,:])
-            try:
-                cslice2=np.transpose(arrays[i][:,c+row_interval,:])
-            except IndexError:
-                cslice2=None
-                WT2=None
+
             if Xunits=='cells':    
                 xlabel='Column'
             else:
@@ -192,24 +170,21 @@ def make_xsections(arrays,watertable,DX,DY,rc_range,row_interval,col_interval,fn
             label='Model row %s; VE: %sx'
             try:
                 WT1 = watertable[c,:]
-                if cslice2 != None:
-                    WT2 = watertable[c+row_interval,:]
-                make_xsectplot(nlayers,cslice1,cslice2,row_interval,c,DX,label,xlabel,colors,WT1=WT1,WT2=WT2,
+
+                make_xsectplot(nlayers,cslice1,row_interval,c,DX,label,xlabel,colors,VE,WT1=WT1,
                                Xunits=Xunits,linewidth=linewidth,WTcolor=WTcolor,WTthick=WTthick)
             except:
-                make_xsectplot(nlayers,cslice1,cslice2,row_interval,c,DX,label,xlabel,colors,Xunits=Xunits,linewidth=linewidth)
+                make_xsectplot(nlayers,cslice1,c,DX,label,xlabel,colors,VE,Xunits=Xunits,
+                               linewidth=linewidth)
             
             fig.savefig()
+            plt.close('all')
         print "\n\nColumns: ",
-        for c in range(ncols)[startcol:endcol:col_interval][::2]:
-            print "%s,%s" %(c+1,c+col_interval+1),
+        for c in range(startcol,endcol,col_interval):
+            print "%s,%s" %(c,c+col_interval),
 
             cslice1=np.transpose(arrays[i][:,:,c])
-            try:
-                cslice2=np.transpose(arrays[i][:,:,c+col_interval])
-            except IndexError:
-                cslice2=None
-                WT2=None
+
             if Xunits=='cells':
                 xlabel='Row'  
             else:
@@ -219,80 +194,85 @@ def make_xsections(arrays,watertable,DX,DY,rc_range,row_interval,col_interval,fn
             
             try:
                 WT1 = watertable[:,c]
-                if cslice2 != None:
-                    WT2 = watertable[:,c+row_interval]
-                make_xsectplot(nlayers,cslice1,cslice2,col_interval,c,DY,label,xlabel,colors,WT1=WT1,WT2=WT2,
+
+                make_xsectplot(nlayers,cslice1,c,DY,label,xlabel,colors,VE,WT1=WT1,
                                Xunits=Xunits,linewidth=linewidth,WTcolor=WTcolor,WTthick=WTthick)
             except:
-                make_xsectplot(nlayers,cslice1,cslice2,col_interval,c,DY,label,xlabel,colors,Xunits=Xunits,linewidth=linewidth)
+                make_xsectplot(nlayers,cslice1,c,DY,label,xlabel,colors,VE,
+                               Xunits=Xunits,linewidth=linewidth)
             fig.savefig()
-        print "\n\nsaving to %s" %(fnames[i])
+
+        print "\n\nsaving to {0}".format(fnames[i])
         fig.close()
 
 # MAIN program
 # input file
-infile="plot_MF_layers.in"
-
-# Get input files locations
-infile=open(infile,'r').readlines()
-inputs=defaultdict()
-for line in infile:
-    try:
-        if "#" not in line.split()[0]:
-            varname=line.split("=")[0]
-            var=line.split("=")[1].split()[0]
-            inputs[varname]=var.replace("'","") # strip extra quotes
-    except IndexError: # means that line is empty
-        continue
-
-modelbottoms=inputs["modelbottoms"]
-L1top=inputs["L1top"]
-xsname=inputs["xsname"]
-colors=inputs["colors"].strip('[]').split(',')
-interval=int(inputs["interval"])
-DISfile=inputs["DIS_file"]
-Xunits=inputs["Xunits"]
-linewidth=float(inputs["linewidth"])
 try:
-    watertable=inputs["watertable"]
-    HDSfile=inputs["HDS_file"]
-    WTcolor=inputs["WTcolor"]
-    WTthick=float(inputs["WTthickness"])
+    infile = sys.argv[1]
 except:
-    watertable='Off'
+    infile = "plot_MF_layers.in"
 
+# Read in the XML input file
+##
+inpardat = ET.parse(infile)
 
+inpars = inpardat.getroot()
+UseDis = tf2flag(inpars.findall('.//layers/UseDis')[0].text)
+if UseDis == False:
+    modelbottoms = inpars.findall('.//layers/modelbottoms')[0].text
+    L1top = inpars.findall('.//layers/L1top')[0].text
+
+xsname=inpars.findall('.//files/xsname')[0].text
+colors = []
+for ccol in inpars.findall('.//colors/color'):
+    colors.append(ccol.text)
+interval=int(inpars.findall('.//options/interval')[0].text)
+DISfile=inpars.findall('.//files/DIS_file')[0].text
+Xunits=inpars.findall('.//options/Xunits')[0].text
+linewidth = float(inpars.findall('.//options/linewidth')[0].text)
+
+startrow = int(inpars.findall('.//rows/startrow')[0].text)
+endrow = int(inpars.findall('.//rows/endrow')[0].text)
+startcol = int(inpars.findall('.//columns/startcolumn')[0].text)
+endcol = int(inpars.findall('.//columns/endcolumn')[0].text)
+
+if inpars.findall('.//options/watertable')[0].text.lower() == "on":
+    watertable = "On"
+    HDSfile=inpars.findall('.//WaterTableSettings/HDS_file')[0].text
+    WTcolor=iinpars.findall('.//WaterTableSettings/WTColor')[0].text
+    WTthick=float(inpars.findall('.//WaterTableSettings/WTthickness')[0].text)
+    # read in water table values
+else:
+    watertable = "Off"
+try:
+    VE = float(inpars.findall('.//options/VE')[0].text)
+except:
+    VE = -999
 
 # get dimmensions
 DX, DY, NLAY, NROW, NCOL, i = discomb_utilities.read_meta_data(DISfile)
 if Xunits=='miles':
     DX,DY = DX/5280.0, DY/5280.0
 
-startrow, endrow, row_interval = set_rowscols(interval, NROW, "rows")
-startcol, endcol, col_interval = set_rowscols(interval, NCOL, "columns")
-
-
-#temp=np.fromfile(L1top,sep=" ")
-#L1top=np.reshape(temp,(NROW,NCOL))
-#bots=np.fromfile(modelbottoms,sep=" ")
+row_interval = interval
+col_interval = interval
 
 # get layer tops/bottoms from DIS file
 layer_elevs=np.zeros((NLAY+1, NROW, NCOL))
 for c in range(NLAY+1):
     tmp, i = discomb_utilities.read_nrow_ncol_vals(DISfile, NROW, NCOL, 'float', i)
     layer_elevs[c, :, :] = tmp
-'''
-#nlayers=np.size(bots)/np.size(L1top)
-bots=np.reshape(bots, (NLAY, NROW, NCOL))
-all=np.append(L1top, bots)
-all=np.reshape(all, (NLAY+1, NROW, NCOL))
-'''
+
+
 if watertable.lower()=='on':
     hds = bf.HeadFile(HDSfile)
     heads = hds.get_data(kstp=1, kper=1)
     watertable = heads[0,:,:]
+else:
+    WTcolor = None
+    WTthick = None
 
-
-make_xsections([layer_elevs],watertable,DX,DY,[startrow,endrow,startcol,endcol],row_interval,col_interval,[xsname],colors,
+make_xsections([layer_elevs],watertable,DX,DY,[startrow,endrow,startcol,endcol],
+               row_interval,col_interval,[xsname],colors,VE,
                Xunits=Xunits,linewidth=linewidth,WTcolor=WTcolor,WTthick=WTthick)
 print "\nDone!"
